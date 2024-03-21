6c60e786b17d71a67962034211b9a80ab30018a5
erickdaniela <danielkewem2018@gmail.com>
2024-03-20 21:43:51 -0300

    erros consertados

--- api/db.js
+++ api/db.js
@@ -4,5 +4,5 @@ export const db = mysql.createConnection({
     host : "localhost",
     user : "root",
     password : "",
-    database : "Crud_Node_Usuarios" 
-})
+    database : "Crud_Node_Usuarios" ,
+});
